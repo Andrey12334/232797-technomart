897900defb8aef4e2263f55422434f9eb2805f0c
Andrey1234 <andrey-kurmaev@rambler.ru>
2018-01-08 18:26:49 +0300

    Java script не работает в explorer 11

--- js/script.js
+++ js/script.js
@@ -1,12 +1,41 @@
 
-(jQuery);
-$(function(){
-  $('.tab-nav li:first').addClass('select');                // Первой вкладке добавляетсякласс select
-  $('.tab-panels>div').hide().filter(':first').show();      // Скрываются все блоки с описанием вкладок кроме первого
-  $('.tab-nav a').click(function(){                         // При клике на вкладку
-    $('.tab-panels>div').hide().filter(this.hash).show();   // - показывается блок с описанием текущей вкладки
-    $('.tab-nav li').removeClass('select');                 // - удаляется класс 'select' у активной ранее вкладки
-    $(this).parent().addClass('select');                    // - добавляется класс 'select' для выбранной вкладки
-    return (false);                                         // - прерывается обработка события onClick
-  })
-})
+var modalbuy = document.querySelector(".modal-buy");
+var modalbuyNotificationClose = document.querySelector(".modal-buy .modal-close");
+var basketItemsamount = document.querySelector(".basket .top-header__amount");
+var bookmarkItemsamount = document.querySelector(".bookmark .top-header__amount");
+var BuyButtons = document.querySelectorAll(".buy");
+var BookmarkButtons = document.querySelectorAll(".bookmarks");
+var Basket = document.querySelector(".basket");
+var Bookmark = document.querySelector(".bookmark"); 
+
+window.addEventListener("keydown", function (event) {
+  if (event.keyCode === 27) {
+    modalbuy.classList.remove("modal-show");
+  }
+});
+
+for (var i = 0; i < BuyButtons.length; i++) {
+  BuyButtons[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    modalbuy .classList.add("modal-show");
+    var value = parseInt(basketItemsamount .innerHTML);
+    value = value + 1;
+    basketItemsamount .innerHTML = (value + "");
+    Basket.classList.add("top-header--active");
+  });
+}
+for (var i = 0; i < BookmarkButtons.length; i++) {
+    BookmarkButtons[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    modalbuy .classList.add("modal-show");
+    var value = parseInt(bookmarkItemsamount .innerHTML);
+    value = value + 1;
+    bookmarkItemsamount .innerHTML = (value + "");
+    Bookmark.classList.add("top-header--active");
+  });
+}
+
+modalbuyNotificationClose.addEventListener("click", function (event) {
+  event.preventDefault();
+  modalbuy.classList.remove("modal-show");
+});
